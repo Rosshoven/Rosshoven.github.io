99aa60e3751d86839c2976f642a9a2fe447aeb41
Rosshoven <Michaeledwardross@gmail.com>
2022-11-22 16:04:13 -0600

    some changes in border, first JS attempt

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-    document.getElementById('view-button-1').onclick = function () {
-        window.location.href = "https://github.com/Rosshoven/Pacmen_Mod_7.git";
-    };
+    // document.getElementById('view-button-1').onclick = function () {
+    //     window.location.href = "https://github.com/Rosshoven/Pacmen_Mod_7.git";
+    // };
+
+    const beethoven = () => {
+     let beethoven = getElementById('left').innerHTML;
+     beethoven = (./Images/Beethoven-Warhol.jpg);
+    }
+
+
+// function beethoven () {
+//         window.location.href="./Images/Beethoven-Warhol.jpg";
+//     };
