9eef95ea325cc05006fc45fcd8b4b55f9427c0d8
Rosshoven <Michaeledwardross@gmail.com>
2023-10-17 16:07:08 -0500

    Eyes

--- script.js
+++ script.js
@@ -2,12 +2,24 @@
     //     window.location.href = "https://github.com/Rosshoven/Pacmen_Mod_7.git";
     // };
 
-    const beethoven = () => {
-     let beethoven = getElementById('left').innerHTML;
-     beethoven = 
-    }
+    // const beethoven = () => {
+    //  let beethoven = getElementById('left').innerHTML;
+    //  beethoven = 
+    // }
 
 
 // function beethoven () {
 //         window.location.href="./Images/Beethoven-Warhol.jpg";
 //     };
+
+const balls = document.getElementsByClassName('ball');
+document.onmousemove = (event) => {
+  const x = (event.clientX * 100) / window.innerWidth + '%';
+  const y = (event.clientY * 100) / window.innerHeight + '%';
+
+  for (let i = 0; i < 2; i++) {
+    balls[i].style.left = x;
+    balls[i].style.top = y;
+    balls[i].transform = 'translate(-' + x + ',-' + y + ')';
+  }
+};
